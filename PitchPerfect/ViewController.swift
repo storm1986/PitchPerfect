7f607c48a592568c7ddb180f5691266711ba6a46
Ler Zhi Yao <storm_1986@hotmail.com>
2019-01-09 23:02:20 +0800

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Ler Zhi Yao on 09/01/2019.
+//  Copyright © 2019 Ler Zhi Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
